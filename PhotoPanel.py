510cad401922554da7c715b95685660e3795622f
Mees Ike <mees.ike@wur.nl>
2025-05-09 17:13:36 +0200

    added PhotoPanel.py which will create a pannel of checkboximages. Right now it makes 1.

---
+++ PhotoPanel.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from CheckBoxImage import CheckBoxImage as CBI
+
+
+class Panel(Frame):
+    def __init__(self):
+        super().__init__()
+
+        self.pack()
+
+        self.checkboximage = CBI(self)
+        self.checkboximage.pack()
+
+        # lets try to build this panel with pack() instead of grid()
+        # grid gives more freedom, but since we have such a specific
+        # environment, pack might work really well here and save us
+        # a lot of time!
+
+        # just figured out that its not allowed to have a child use pack() when parent uses grid()
+        # which is really fucking annoying but also understanding :(
+
+        # we need the following:
+        # - data structure that lets us visualize an image over and over
+        # - figure out best tk datatype for a highlightable image
+
+        # constraints
+        # - everything selected and moved to another "panel" has
+        #   consequences for the decisions.json. so at the end the json
+        #   should be modified, after which the images are deleted
