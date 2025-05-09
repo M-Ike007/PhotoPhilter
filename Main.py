4c7569ad425e55c73b5da6f8b17102c942c17822
Mees Ike <mees.ike@wur.nl>
2025-05-09 17:56:45 +0200

    add third GUI to Main.py file

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import Setup as Setup
 import PhotoPhilter as Pp
-
+import FinalSort as Fs
 # TODO technically it would make more sense if all JSON files are made in Main.py.
 #  That would mean settings.json and future JSONs.
 
@@ -9,3 +9,6 @@ setup.mainloop()
 
 app = Pp.App()
 app.mainloop()
+
+final_sort = Fs.App()
+final_sort.mainloop()
