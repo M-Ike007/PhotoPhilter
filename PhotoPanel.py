abf92d16a15eb438627146780aca2884753126af
ike004 <mees2duizend@gmail.com>
2025-12-07 14:53:30 +0100

    panel now automatically loads all images and creates amount of checkboximage objects

--- PhotoPanel.py
+++ PhotoPanel.py
@@ -4,13 +4,14 @@ import json
 
 
 class Panel(Frame):
-    def __init__(self):
+    def __init__(self, image_list: str):
         super().__init__()
 
         self.grid()
 
-        self.checkboximage = CBI(self, image_path=self.set_checkbox_image('discard'))
-        self.checkboximage.grid()
+        self.checkboxes = self.create_checkbox_list(image_list)
+        self.place_checkboxes()
+
 
     def set_checkbox_image(self, list_name: str):
         # unpack decisions.json information
@@ -32,6 +33,35 @@ class Panel(Frame):
 
         return image_path
 
+    def create_checkbox_list(self, list_name: str):
+        # unpack decisions.json information
+        file = open("decisions.json")
+        decisions = file.read()
+        file.close()
+
+        # get list of list_name
+        imagenames = json.loads(decisions)[list_name]
+
+        # unpack settings.json and get directory
+        file = open('settings.json')
+        settings = file.read()
+        file.close()
+        directory = json.loads(settings)['directory']
+
+        checkboxes = []
+        for i in range(len(imagenames)):
+            image_path = str(directory) + '/' + str(imagenames[i])
+            checkboxes.append(CBI(self, image_path=image_path))
+        return checkboxes
+
+    def place_checkboxes(self):
+        for i in range(len(self.checkboxes)):
+            self.checkbox = self.checkboxes[i]
+            self.checkbox.grid(row=(1 + (i%7)), column=(i//7 +1))
+            # the number 7 is based on my personal laptop screen size and might be different elsewhere
+
 if __name__ == '__main__':
-    panel = Panel()
-    panel.set_checkbox_image('discard')
+    panel = Panel('keep')
+    panel.place_checkboxes()
+
+
