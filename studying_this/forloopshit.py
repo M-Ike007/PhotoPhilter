c2afd09094b7e35ebd4de481af77048cb4277f0d
ike004 <mees2duizend@gmail.com>
2026-01-02 23:58:45 +0100

    practice

--- studying_this/forloopshit.py
+++ studying_this/forloopshit.py
@@ -1,5 +1,10 @@
-import math
+import tkinter as tk
 
-for i in range(100):
-    print('row:', (1 + (i%7)),
-          'column:', (i//7 +1))
+window = tk.Tk()
+button1 = tk.Button(text="B1", width=50)
+button2 = tk.Button(text="B2", width=20)
+button3 = tk.Button(text="B3", width=50)
+button1.grid(column=0, row=0)
+button2.grid(column=0, row=1, sticky="W")
+button3.grid(column=0, row=2)
+window.mainloop()
