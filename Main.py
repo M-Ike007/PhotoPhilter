5781d43850359f0193272e33ce64314908a8b10e
ike004 <mees2duizend@gmail.com>
2026-01-13 00:14:48 +0100

    add TODO

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ import FinalSort as Fs
 # TODO technically it would make more sense if all JSON files are made in Main.py.
 #  That would mean settings.json and future JSONs.
 
+# TODO extract a class that allows for easier communication with JSON files.
+#  There's a lot of redundant code with the JSON Files
 setup = Setup.App()
 setup.mainloop()
 
