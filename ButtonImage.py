21e017eda6b4143e5627ed8e178db5b1553295f2
ike004 <mees2duizend@gmail.com>
2026-01-13 00:55:05 +0100

    add two image states

--- ButtonImage.py
+++ ButtonImage.py
@@ -3,14 +3,18 @@ from PIL import Image, ImageTk, ImageFilter
 
 
 class ButtonImage(tk.Button):
-    def __init__(self, master, image_path, parent_panel_name):
+    def __init__(self, master, image_path):
         self.image_path = image_path
         self.state = False
         # load image
-        self.image = ImageTk.PhotoImage(Image.open(self.image_path).resize((100, 100)))
+        self.image = Image.open(self.image_path).resize((100, 100))
+        # image normal
+        self.image_unclicked = ImageTk.PhotoImage(self.image)
+        # image clicked
+        self.image_clicked = ImageTk.PhotoImage(self.image.filter(ImageFilter.GaussianBlur(radius=3)))
 
         super().__init__(master,
-                         image=self.image,
+                         image=self.image_unclicked,
                          height=100,
                          width=100,
                          padx=0,
@@ -21,16 +25,21 @@ class ButtonImage(tk.Button):
     def clicked(self):
         self.state = not self.state
         if self.state:
-            print('-', self.image_path)
+            info = str('-'+ self.image_path)
+            self.config(image=self.image_clicked)
         else:
-            print('+', self.image_path)
+            info = str('+'+ self.image_path)
+            self.config(image=self.image_unclicked)
+        print(info)
+        return info
+
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Custom button Example")
     label = tk.Label(root, text="click me")
     label.pack()
-    button = ButtonImage(root, image_path='Media/004.JPG', parent_panel_name='discard')
+    button = ButtonImage(root, image_path='Media/004.JPG')
     button.pack()
     # custom_checkbox = CheckBoxImage(root)
     # custom_checkbox.pack(padx=20, pady=20)
